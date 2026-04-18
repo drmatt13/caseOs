543e2362dd3f80efde16f10a39ca5d8380e967ab
Matthew Sweeney <Matthew.Sweeney001@gmail.com>
2026-04-18 10:35:54 -0400

    4/18

--- local-dev-server/src/invokeAsyncLambdaFunctions.ts
+++ local-dev-server/src/invokeAsyncLambdaFunctions.ts
@@ -1,31 +1,6 @@
-import {
-  DeleteMessageCommand,
-  ReceiveMessageCommand,
-  SQSClient,
-  type Message,
-} from "@aws-sdk/client-sqs";
+// import { <newFunction> } from "@repo/event-replay"
 
 // Asynchronous Lambda Functions
 import { lambdaHandler as cognitoPostConfirmationTrigger } from "../../cdk-app/lambda_functions/cognito-post-confirmation-trigger";
-
-type S3EventRecord = {
-  eventSource: string;
-  eventName: string;
-  s3?: {
-    bucket?: { name?: string };
-    object?: { key?: string };
-  };
-};
-
-type S3EventMessageBody = {
-  Records?: S3EventRecord[];
-};
-
-const region = process.env.AWS_REGION ?? "us-east-1";
-const queueUrl = process.env.DEV_LAMBDA_REPLAY_QUEUE_URL ?? "";
-
-const sqsClient = new SQSClient({
-  region,
-});
 
 export default function invokeAsyncLambdaFunctions() {}
